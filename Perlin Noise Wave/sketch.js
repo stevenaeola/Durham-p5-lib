c63a6c2441659be998bbbcc4fea23c18a34a4263
Ibrahim Mizi <ibmiz@users.noreply.github.com>
2018-12-03 09:36:43 +0000

    Changed code to use a class for a drawable line

--- Perlin Noise Wave/sketch.js
+++ Perlin Noise Wave/sketch.js
@@ -1,46 +1,51 @@
-function setup(){
-    createCanvas(windowWidth,windowHeight);
-    fill(240);
-    noStroke();
-}
-
-ofs =0;
-ofs_v =1;
-
-function draw() {
-    background('black');
-    ofs+=ofs_v;
-
-    if((ofs==offset) || (ofs==0))
-    {
-        ofs_v=0-ofs_v;
-    }
-
-    strokeWeight(6);
-    drawLine(212+ofs,to = color('#DB028C'),from = color('#FFAE34'));
-    strokeWeight(1);
-
-}
-
+var line1;
+var ofs =0;
+var ofs_v =1;
 var step = 80;
 var noiseScale = 0.02;
 var offset = 300;
 
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    fill(240);
+    noStroke();
+		line1 = new Line(212+ofs,to = color('#DB028C'),from = color('#FFAE34'));
+}
 
-function drawLine(y0,to,from){
+
+function draw() {
+	background('black');
+	ofs+=ofs_v;
+	if((ofs==offset) || (ofs==0)){ofs_v=0-ofs_v;}
+	strokeWeight(5);
+	line1.drawLine();
+
+}
+
+//Class to create Lines
+class Line {
+	constructor (y0, to, from){
+		this.y0=y0;
+		this.to=to;
+		this.from=from;
+	}
+	
+	
+	drawLine(){
     fill(255,4);
     beginShape();
-    curveVertex(-50,y0);
+    curveVertex(-50,this.y0);
 
     for (var i =0 ; i<width/step+3;i++){
-        var noiseVal = noise(i*noiseScale*(y0*0.06), frameCount*noiseScale);
-        stroke(lerpColor(from,to,noiseVal));
-        curveVertex(i*step-10,y0+noiseVal*offset);
+        var noiseVal = noise(i*noiseScale*(this.y0*0.06), frameCount*noiseScale);
+        stroke(lerpColor(this.from,this.to,noiseVal));
+        curveVertex(i*step-10,this.y0+noiseVal*offset);
     }
 
     curveVertex(width+10, height+200);
     curveVertex(0, height+210);
     curveVertex(0, height+210);
     endShape();
-
+	}
+	
 }
