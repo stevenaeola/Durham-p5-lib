334fd9ee9aa7b7a8bfc6e7783307255a84bc6f90
JakeSESaunders <JakeSESaunders@outlook.com>
2018-12-04 21:58:12 +0000

    Converted from Processing to p5

--- Pond/pond.js
+++ Pond/pond.js
@@ -1,57 +1,53 @@
-ArrayList fish = new ArrayList();
+var fish = [];
 
-void setup() {
-  fullScreen();
+function setup() {
+  createCanvas(displayWidth, displayHeight);
   /* Gives a nice glow feel */
   strokeWeight(10);
   /* 
-   IMPORTANT: because we are using vertex's to make our fish, the line joining becomes spiky when the 
+   IMPORTANT: because we are using vertices to make our fish, the line joining becomes spiky when the 
    strokeWeight is bigger than 1. 
    */
   strokeJoin(ROUND);
   stroke(0, 150, 255);
   /* Add 100 fish */
-  for (int i = 0; i < 60; i++) {
-    fish.add(new Fish());
+  for (i = 0; i < 60; i++) {
+    fish.push(new Fish());
   }
 }
 
-void draw() {
+function draw() {
   fill(0, 30);
   rect(-10, -10, width+20, height+20);
   fill(0, 150, 255, 20);
-
-  for (int i = 0; i < fish.size (); i++) {
-    Fish f = (Fish) fish.get(i);
+  
+  for (var i = 0; i < fish.length; i++) {
+    var f = fish[i];
     f.draw();
     f.boundaries();
   }
 }
 
 class Fish {
-  PVector loc;
-  PVector vel;
-  /* Just to add some individuality to the fish wiggle */
-  float s = random(-90, 90);
-  float d = random(0.1, 0.3);
+    constructor() {
+        this.loc = createVector(random(width), random(height));
+        /* Make a random velocity */
+        this.vel = createVector(random(-1, 1), random(-1, 1));
+        this.s = random(-90, 90);
+        this.d = random(0.1, 0.3);
+    }
 
-  Fish() {
-    loc = new PVector(random(width), random(height));
-    /* Make a random velocity */
-    vel = new PVector(random(-1, 1), random(-1, 1));
-  }
-
-  void draw() {
-    loc.add(vel);
-    pushMatrix();
-    translate(loc.x, loc.y);
-    scale(d);
+  draw() {
+    this.loc.add(this.vel);
+    push();
+    translate(this.loc.x, this.loc.y);
+    scale(this.d);
     /* Get the direction and add 90 degrees. */
-    rotate(vel.heading2D()-radians(90));
+    rotate(this.vel.heading()-radians(90));
     beginShape();
-    for (int i = 0; i <= 180; i+=20) {
-      float x = sin(radians(i)) * i/3;
-      float angle = sin(radians(i+s+frameCount*5)) * 50;
+    for (i = 0; i <= 180; i+=20) {
+      var x = sin(radians(i)) * i/3;
+      var angle = sin(radians(i+this.s+frameCount*5)) * 50;
       vertex(x-angle, i*2);
       vertex(x-angle, i*2);
     }
@@ -60,22 +56,22 @@ class Fish {
      Otherwise un ugly line will appear down the middle. To see what I mean uncomment the below line and comment
      the other line.
      */
-    for (int i = 180; i >= 0; i-=20) {
-      //for (int i = 0; i < 180; i+=20){
-      float x = sin(radians(i)) * i/3;
-      float angle = sin(radians(i+s+frameCount*5)) * 50;
+    for (i = 180; i >= 0; i-=20) {
+      //for (i = 0; i < 180; i+=20){
+      x = sin(radians(i)) * i/3;
+      angle = sin(radians(i+this.s+frameCount*5)) * 50;
       vertex(-x-angle, i*2);
       vertex(-x-angle, i*2);
     }
     endShape();
-    popMatrix();
+    pop();
   }
 
-  void boundaries() {
+  boundaries() {
     /* Instead of changing the velocity when the fish  */
-    if (loc.x < -100) loc.x = width+100;
-    if (loc.x > width+100) loc.x = -100;
-    if (loc.y < -100) loc.y = height+100;
-    if (loc.y > height+100) loc.y = -100;
+    if (this.loc.x < -100) this.loc.x = width+100;
+    if (this.loc.x > width+100) this.loc.x = -100;
+    if (this.loc.y < -100) this.loc.y = height+100;
+    if (this.loc.y > height+100) this.loc.y = -100;
   }
 }
