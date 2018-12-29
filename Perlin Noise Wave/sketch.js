151a76ef318a257c89a580e7b8808ff8e2a96510
Ibrahim Mizi <ibmiz@users.noreply.github.com>
2018-12-29 15:00:09 +0000

    Added button to change background and fixed sliders

--- Perlin Noise Wave/sketch.js
+++ Perlin Noise Wave/sketch.js
@@ -7,6 +7,10 @@ var ofs = 0.0;
 var sliderx;
 var slidery;
 
+var bgcolour;
+var colourbutton;
+var backgroundcolour;
+
 function setup(){
   createCanvas(windowWidth,windowHeight);
   stroke(255, 150);
@@ -21,23 +25,43 @@ function setup(){
   sliderx.style('width', '80px');
   
   slidery = createSlider(0, height, 100);
-  slidery.position(10, 30);
+  slidery.position(10, 40);
   slidery.style('width', '80px');
   
   slidercolour1 = createSlider(0, 255, 100);
-  slidercolour1.position(10, 50);
+  slidercolour1.position(10, 70);
   slidercolour1.style('width', '80px');
   
   slidercolour2 = createSlider(0, 255, 100);
-  slidercolour2.position(10, 70);
+  slidercolour2.position(10, 100);
   slidercolour2.style('width', '80px');
+	
+	bgcolour= "white";
+	backgroundcolour = createInput("Enter background colour");
+	backgroundcolour.position(200,20);
+	backgroundcolour.size(150);
+	
+	colourbutton = createButton("Change colour");
+	colourbutton.position(backgroundcolour.x+ backgroundcolour.width,20);
+	colourbutton.mousePressed(changeColour);
+	
 }
 
+function changeColour(){
+	bgcolour = backgroundcolour.value();
+}
 
 function draw() {
-	background('black');
+	background(bgcolour);
+	
   ofs += 0.015;
   var waveH = map(sliderx.value(), 0, width, 100, 500);
+	
+	text("Wave width", sliderx.x * 2 + sliderx.width, 20);
+	text("Wave Height", slidery.x * 2 + slidery.width, 50);
+	text("Colour 1", slidercolour1.x * 2 + slidercolour1.width, 80);
+	text("Colour 2", slidercolour2.x * 2 + slidercolour2.width, 110);
+	
   for (var h = slidery.value(); h < height + 100; h += 4) {
 		line = new Line (slidery.value(), sliderx.value(), noiseX, noiseY, noiseF, ofs, h, waveH,slidercolour1.value(), slidercolour2.value());
 		line.drawLine();
