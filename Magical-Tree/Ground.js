1aa446805b201e4cc5bbb24160621e50d0ded391
Reuben Elijah McQueen <44359651+remcqueen1999@users.noreply.github.com>
2019-01-16 23:00:02 +0000

    working optional renderer

--- Magical-Tree/Ground.js
+++ Magical-Tree/Ground.js
@@ -1,27 +1,25 @@
 class clGround {
 
-  constructor(argRenderer) {
+  constructor() {
     this.n = 0;
-    if (argRenderer != undefined) {
-      renderer.this = argRenderer;
-    }
   }
 
-  draw() {
+  draw(g) {
     var i = 0;
-    if (this.renderer != undefined) {
-      this.renderer.noLoop();
-      this.renderer.noStroke();
-      this.renderer.fill(20);
-      this.renderer.beginShape();
-      this.renderer.vertex(0, height);
+    if (g) {
+      var renderer = createGraphics(width, height);
+      renderer.noLoop();
+      renderer.noStroke();
+      renderer.fill(20);
+      renderer.beginShape();
+      renderer.vertex(0, height);
       for (i; i <= width; i += 50) {
-        this.renderer.vertex(i, this.renderer.map(noise(this.n), 0, 1, height - 30, height));
+        renderer.vertex(i, renderer.map(noise(this.n), 0, 1, height - 30, height));
         this.n += 0.1;
       }
-      this.renderer.vertex(width, height);
-      this.renderer.endShape();
-      image(this.renderer, 0, 0);
+      renderer.vertex(width, height);
+      renderer.endShape();
+      g.image(renderer, 0, 0);
       
     } else {
       noLoop();
