25ed02529e8a68339b7e5973a77f5414929517ec
Reuben Elijah McQueen <44359651+remcqueen1999@users.noreply.github.com>
2019-01-16 09:57:44 +0000

    included optional renderer

--- Magical-Tree/Ground.js
+++ Magical-Tree/Ground.js
@@ -1,21 +1,43 @@
 class clGround {
 
-  constructor() {
+  constructor(argRenderer) {
     this.n = 0;
+    if (argRenderer != undefined) {
+      renderer.this = argRenderer;
+    }
   }
 
   draw() {
-    noLoop();
-    noStroke();
-    fill(20);
-    beginShape();
-    vertex(0, height);
-    for (var i = 0; i <= width; i += 50) {
-      vertex(i, map(noise(this.n), 0, 1, height - 30, height));
-      this.n += 0.1;
+    var i = 0;
+    if (this.renderer != undefined) {
+      this.renderer.noLoop();
+      this.renderer.noStroke();
+      this.renderer.fill(20);
+      this.renderer.beginShape();
+      this.renderer.vertex(0, height);
+      for (i; i <= width; i += 50) {
+        this.renderer.vertex(i, this.renderer.map(noise(this.n), 0, 1, height - 30, height));
+        this.n += 0.1;
+      }
+      this.renderer.vertex(width, height);
+      this.renderer.endShape();
+      image(this.renderer, 0, 0);
+      
+    } else {
+      noLoop();
+      noStroke();
+      fill(20);
+      beginShape();
+      vertex(0, height);
+      for (i; i <= width; i += 50) {
+        vertex(i, map(noise(this.n), 0, 1, height - 30, height));
+        this.n += 0.1;
+      }
+      vertex(width, height);
+      endShape();
     }
-    vertex(width, height);
-    endShape();
+    
+    
   }
 
 }
