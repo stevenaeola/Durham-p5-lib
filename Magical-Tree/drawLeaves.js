57163ea0c59a65054adaee5034acb86d4afecf0f
Reuben Elijah McQueen <44359651+remcqueen1999@users.noreply.github.com>
2019-01-16 23:01:31 +0000

    added optional renderer

--- Magical-Tree/drawLeaves.js
+++ Magical-Tree/drawLeaves.js
@@ -1,17 +1,13 @@
 class clDrawLeaves {
 
-  constructor(argRenderer, leafArray = []) {
-    if (argRenderer != undefined) {
-      this.renderer = argRenderer;
-    }
+  constructor(leafArray = []) {
     this.leafs = leafArray;
     this.randomColor = true;
     this.minHue = 0;
     this.maxHue = 0;
   }
 
-  genLeaves(minDiam, maxDiam, minAlpha, maxAlpha) {
-
+  genLeaves(minDiam, maxDiam, minAlpha, maxAlpha, g) {
     var minHue;
     var maxHue;
     if (this.randomColor) {
@@ -25,6 +21,7 @@ class clDrawLeaves {
     }
 
     for (var i = 0; i < this.leafs.length; i++) {
+      
       var h = map(i, 0, this.leafs.length, minHue, maxHue);
       var s = 255;
       var b = 255;
@@ -34,7 +31,9 @@ class clDrawLeaves {
       var jitterX = random(-30, 30);
       var jitterY = random(-30, 30);
       
-      if (this.renderer != undefined) {
+      if (g) {
+        this.renderer.colorMode(HSB);
+        this.renderer.noStroke();
         this.renderer.fill(h, s, b, a);
         this.renderer.ellipse(this.leafs[i].x + jitterX, this.leafs[i].y + jitterY, diam, diam);
       
@@ -47,12 +46,19 @@ class clDrawLeaves {
 
 
 
-  draw() {
-    this.genLeaves(0, 90, 0, 0.03); // big leaves
-    this.genLeaves(0, 15, 0, 0.25); // small leaves
+  draw(g) {
+    if (g) {
+      this.renderer = createGraphics(width, height);
+      this.renderer.clear();
+      this.genLeaves(0, 90, 0, 0.03, g); // big leaves
+      this.genLeaves(0, 15, 0, 0.25, g); // small leaves
+    } else {
+      this.genLeaves(0, 90, 0, 0.03); // big leaves
+      this.genLeaves(0, 15, 0, 0.25); // small leaves
+    }
     
-    if (this.renderer != undefined) {
-      image(this.renderer, 0, 0)
+    if (g) {
+      g.image(this.renderer, 0, 0)
     }
   }
 
@@ -61,7 +67,6 @@ class clDrawLeaves {
     this.maxHue = argMaxHue;
     this.randomColor = false;
    
-
   }
 
   getColor() {
@@ -73,16 +78,24 @@ class clDrawLeaves {
 
   }
 
-  drawFruit() {
-    this.genLeaves(0, 12, 0, 0.60);
-    if (this.renderer != undefined) {
-      image(this.renderer, 0, 0)
+  drawFruit(g) {
+    if (g) {
+      this.genLeaves(0, 12, 0, 0.60, g);
+    } else {
+      this.genLeaves(0, 12, 0, 0.60);
+    }
+    if (g) {
+      g.image(this.renderer, 0, 0);
     }
   }
 
 
   deleteLeaf() {
     this.leafs.splice(0, this.leafs.length);
+  }
+
+  setRandomColor(argBool) {
+    this.randomColor = argBool;
   }
 
 }
