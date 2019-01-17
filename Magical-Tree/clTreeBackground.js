de1074d728d30f6aa4571b604c0232c45c5e4282
Reuben Elijah McQueen <44359651+remcqueen1999@users.noreply.github.com>
2019-01-17 13:51:58 +0000

    Update and rename treeBackground.js to clTreeBackground.js

--- Magical-Tree/clTreeBackground.js
+++ Magical-Tree/clTreeBackground.js
@@ -1,17 +1,21 @@
 class clTreeBackground {
 
   constructor() {
+    //background drawn on renderer ofscreen
     this.bg = createGraphics(width, height);
   }
 
   draw(g) {
 
     noLoop();
+    //generates background
     this.bg.noStroke();
     for (this.diam = 1.5 * width; this.diam > 0.5 * width; this.diam -= 20) {
-        this.bg.fill(map(this.diam, 0.5 * width, 1.5 * width, 255, 110)); // changed this to make the gradient more pronounced
+        //changed this to make the gradient more pronounced
+        this.bg.fill(map(this.diam, 0.5 * width, 1.5 * width, 255, 110));
         this.bg.ellipse(width / 2, height / 2, this.diam, this.diam);
     }
+    //if statement for if to draw to canvas or optional renderer
     if (g) {
       g.image(this.bg, 0, 0);
     } else {
