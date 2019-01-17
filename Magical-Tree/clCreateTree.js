2ac52ea61f3efad93ea9d99ccd6ebad56718d2fe
Reuben Elijah McQueen <44359651+remcqueen1999@users.noreply.github.com>
2019-01-17 13:51:25 +0000

    Update and rename createTree.js to clCreateTree.js

--- Magical-Tree/clCreateTree.js
+++ Magical-Tree/clCreateTree.js
@@ -9,22 +9,26 @@ class clCreateTree {
     this.treeAge = 70;
   }
 
+  //creates tree
   sketch() {
+    var i = 0;
     this.tree.beginShape();
     this.tree.noStroke();
     this.tree.background(0, 0);
 
-    for (var i = 0; i < this.treeDensity; i++) {
+
+    for (i; i < this.treeDensity; i += 1) {
       this.tree.fill(map(i, 0, 2, 60, 20));
       this.branch(width / 2, height, this.treeAge, -HALF_PI, this.treeHeight, 0);
     }
     this.tree.endShape();
   }
-
+  
   draw(g) {
     this.tree.noLoop();
     this.tree.clear();
     this.sketch();
+    //checks for optional parameter
     if (g) {
       g.image(this.tree, 5, 5);
     } else {
@@ -33,11 +37,14 @@ class clCreateTree {
 
   }
 
-
-  branch(x, y, bSize, theta, bLength, pos) {
-
-    this.n += 0.01;
+  //recursive branch method
+  branch(argX, argY, bSize, theta, bLength, pos) {
+    var x = argX;
+    var y = argY;
     var diam = lerp(bSize, 0.7 * bSize, pos / bLength);
+    var drawLeftBranch;
+    var drawRightBranch;
+    this.n += 0.01;
     diam *= map(noise(this.n), 0, 1, 0.4, 1.6);
 
     this.tree.ellipse(x, y, diam, diam);
@@ -48,13 +55,17 @@ class clCreateTree {
         this.branch(x, y, bSize, theta, bLength, pos + 1);
       } else {
         this.leafs.push(createVector(x, y));
-        var drawLeftBranch = random(1) > 0.1;
-        var drawRightBranch = random(1) > 0.1;
-        if (drawLeftBranch) this.branch(x, y, random(0.5, 0.7) * bSize, theta - random(PI / 15, PI / 5), random(0.6, 0.8) * bLength, 0);
-        if (drawRightBranch) this.branch(x, y, random(0.5, 0.7) * bSize, theta + random(PI / 15, PI / 5), random(0.6, 0.8) * bLength, 0);
+        drawLeftBranch = random(1) > 0.1;
+        drawRightBranch = random(1) > 0.1;
+        if (drawLeftBranch) {
+ this.branch(x, y, random(0.5, 0.7) * bSize, theta - random(PI / 15, PI / 5), random(0.6, 0.8) * bLength, 0);
+}
+        if (drawRightBranch) {
+ this.branch(x, y, random(0.5, 0.7) * bSize, theta + random(PI / 15, PI / 5), random(0.6, 0.8) * bLength, 0);
+}
 
         if (!drawLeftBranch && !drawRightBranch) {
-          this.tree.push()
+          this.tree.push();
           this.tree.translate(x, y);
           this.tree.rotate(theta);
           this.tree.quad(0, -diam / 2, 2 * diam, -diam / 6, 2 * diam, diam / 6, 0, diam / 2);
@@ -64,31 +75,37 @@ class clCreateTree {
     }
   }
 
+  //getter for leaf array
   get leafArray() {
-    return this.leafs
+    return this.leafs;
   }
 
-
+  //setter for tree height
   setHeight(tHeight) {
     this.treeHeight = tHeight;
   }
 
+  //setter for tree density
   setDensity(tDensity) {
     this.treeDensity = tDensity;
   }
 
+  //setter for tree age
   setAge(tAge) {
     this.treeAge = tAge;
   }
 
+  //getter for tree height
   getHeight() {
     return this.treeHeight;
   }
 
+  //getter for tree density
   getDensity() {
     return this.treeDensity;
   }
 
+  //getter for tree age
   getAge() {
     return this.treeAge;
   }
