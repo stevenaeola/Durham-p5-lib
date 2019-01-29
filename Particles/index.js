289665bef496af0bf6b13fdd9ac64487192a6e77
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-01-29 11:38:24 +0000

    Update index.js

--- Particles/index.js
+++ Particles/index.js
@@ -1,34 +1,11 @@
-//links html rgb sliders to js
-let rSlider = document.getElementById("Red");
-let Routput = document.getElementById("RedD");
-let gSlider = document.getElementById("Green");
-let Goutput = document.getElementById("GreenD");
-let bSlider = document.getElementById("Blue");
-let Boutput = document.getElementById("BlueD");
+var x;
 
-//performs live updates of r,g,b slider values
-Routput.innerHTML = rSlider.value;
-rSlider.oninput = function() {
-	Routput.innerHTML = this.value;
-};
-
-Goutput.innerHTML = gSlider.value;
-gSlider.oninput = function() {
-	Goutput.innerHTML = this.value;
-};
-
-Boutput.innerHTML = bSlider.value;
-bSlider.oninput = function() {
-	Boutput.innerHTML = this.value;
-};
-
-//calls Particles class, intiialising it
-function setup(){
-	particles = new Particles(500,500,500,false);
-	particles.init();
+//Setup function
+function setup(bgcol, txtcol) {
+  x = new Particles(bgcol, txtcol);
 }
 
-// draws particle effect
-function draw(){
-	particles.draw();
+//Draw function
+function draw(g) {
+    x.draw(g);
 }
